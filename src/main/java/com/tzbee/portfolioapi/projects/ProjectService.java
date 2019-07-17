3409d85d2f6aeb6491717e4315adb6ea2d4efff8
tzbee <aminetolba29@gmail.com>
2019-07-17 12:26:02 +0300

    Cleanup

--- src/main/java/com/tzbee/portfolioapi/projects/ProjectService.java
+++ src/main/java/com/tzbee/portfolioapi/projects/ProjectService.java
@@ -3,7 +3,6 @@ package com.tzbee.portfolioapi.projects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
