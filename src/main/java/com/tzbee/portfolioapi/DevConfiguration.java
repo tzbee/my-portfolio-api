99dd92c81fa982d8feac34763341b4819010ee32
tzbee <aminetolba29@gmail.com>
2019-07-20 15:24:39 +0300

    Add dev CORS configuration

---
+++ src/main/java/com/tzbee/portfolioapi/DevConfiguration.java
@@ -0,0 +1,23 @@
+package com.tzbee.portfolioapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@Profile("dev")
+public class DevConfiguration {
+    @Bean
+    public WebMvcConfigurer configurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/projects").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+}
+
